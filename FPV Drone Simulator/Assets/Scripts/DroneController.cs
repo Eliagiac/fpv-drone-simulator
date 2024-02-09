35de200c0772ab3338d7bf2c6f0396b11385debd
Eliagiac <elia.giaccardi@gmail.com>
2024-02-09 13:59:23 +0100

    Added lift off mechanics
    Added physics-based movement to lift the drone off. It does not yet accept user input, and needs stabilizing.

--- FPV Drone Simulator/Assets/Scripts/DroneController.cs
+++ FPV Drone Simulator/Assets/Scripts/DroneController.cs
@@ -5,5 +5,28 @@ using UnityEngine;
 
 public class DroneController : MonoBehaviour
 {
-    
+    public float MotorSpeed = 100f;
+
+    [SerializeField] private Transform _frontLeftMotor;
+    [SerializeField] private Transform _frontRightMotor;
+    [SerializeField] private Transform _backLeftMotor;
+    [SerializeField] private Transform _backRightMotor;
+
+    private Rigidbody _rb;
+
+    private Vector3 _upDirection;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _upDirection = transform.forward;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddForceAtPosition(_upDirection * MotorSpeed * Time.fixedDeltaTime, _frontLeftMotor.position);
+        _rb.AddForceAtPosition(_upDirection * MotorSpeed * Time.fixedDeltaTime, _frontRightMotor.position);
+        _rb.AddForceAtPosition(_upDirection * MotorSpeed * Time.fixedDeltaTime, _backLeftMotor.position);
+        _rb.AddForceAtPosition(_upDirection * MotorSpeed * Time.fixedDeltaTime, _backRightMotor.position);
+    }
 }
