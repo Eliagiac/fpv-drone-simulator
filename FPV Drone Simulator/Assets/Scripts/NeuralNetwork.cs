61cedcde93617f7b7fd188cd513ae95b6d8330c0
Eliagiac <elia.giaccardi@gmail.com>
2024-02-11 15:43:15 +0100

    Implemented FeedForward function

--- FPV Drone Simulator/Assets/Scripts/NeuralNetwork.cs
+++ FPV Drone Simulator/Assets/Scripts/NeuralNetwork.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -30,8 +31,31 @@ public class NeuralNetwork : MonoBehaviour
     }
 
 
-    public void FeedForward()
+    public void FeedForward(double[] inputs)
     {
+        if (inputs.Length != Nodes.Length) throw new ArgumentException(
+            "The size of the inputs array does not match the size of the first layer of the network!");
 
+        Nodes[0] = inputs;
+
+        for (int i = 1; i < Nodes.Length; i++) 
+        {
+            for (int j = 0; j < Nodes[i].Length; j++)
+            {
+                double weightedSum = Biases[i][j];
+
+                for (int k = 0; k < Nodes[i - 1].Length; k++)
+                {
+                    weightedSum += Nodes[i - 1][k] * Weights[i][j][k];
+                }
+
+                Nodes[i][j] = ActivationFunction(weightedSum);
+            }
+        }
+    }
+
+    private double ActivationFunction(double input)
+    {
+        return input;
     }
 }
