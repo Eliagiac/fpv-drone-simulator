cab607d280b0f8926afcb394736019d5831b06b7
Eliagiac <elia.giaccardi@gmail.com>
2024-02-11 15:18:37 +0100

    Implemented neural network initialization

--- FPV Drone Simulator/Assets/Scripts/NeuralNetwork.cs
+++ FPV Drone Simulator/Assets/Scripts/NeuralNetwork.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class NeuralNetwork : MonoBehaviour
 {
     public double[][] Nodes;
-    public double[][] Bias;
+    public double[][] Biases;
     public double[][][] Weights;
 
-    public NeuralNetwork() 
-    { 
+    public NeuralNetwork(int[] size) 
+    {
+        int layers = size.Length;
+
+        Nodes = new double[layers][];
+        Biases = new double[layers][];
+        Weights = new double[layers][][];
+
+        for (int i = 0; i < layers; i++)
+        {
+            Nodes[i] = new double[size[i]];
+            Biases[i] = new double[size[i]];
+            Weights[i] = new double[size[i]][];
+
+            if (i > 0)
+            {
+                for (int j = 0; j < size[i]; j++) Weights[i][j] = new double[size[i - 1]];
+            }
+        }
+    }
+
+
+    public void FeedForward()
+    {
 
     }
 }
