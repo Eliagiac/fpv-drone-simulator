e69f2762cc6cad6f71b820340ac93dfaeec55a63
Eliagiac <elia.giaccardi@gmail.com>
2024-02-21 16:15:51 +0100

    Improved checkpoints layout
    Also increased network size.

--- FPV Drone Simulator/Assets/Scripts/AIController.cs
+++ FPV Drone Simulator/Assets/Scripts/AIController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class AIController : DroneController
 {
-    public static readonly int[] NetworkSize = { 20, 16, 12, 10, 8, 4 };
+    public static readonly int[] NetworkSize = { 20, 16, 12, 12, 10, 8, 4 };
 
     public NeuralNetwork NeuralNetwork;
 
